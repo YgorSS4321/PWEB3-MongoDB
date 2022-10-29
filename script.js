75c42ebf5ec10d7dd6724ce83dd0064cc76d7bbf
Marcos  Felype <103426684+MarcosFelype@users.noreply.github.com>
2022-10-29 18:08:06 -0300

    corrigindo arquivos direcionados de modo errôneo

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+<<<<<<< HEAD
 var element = document.getElementById("createColumn");
 element.addEventListener("click", nameColumn);
 var mainSection = document.getElementById("main");
 var tasksDatabase = [];
+=======
+import {listaColunas, Coluna, ToDo} from "/classeToDo.js";
+
+var element = document.getElementById("createColumn");
+element.addEventListener("click", nameColumn);
+var mainSection = document.getElementById("main");
+>>>>>>> parent of c1061ca (commit)
 var localStorageIndex = 0;
 var i = 0;
 var j = 0;
@@ -10,6 +18,16 @@ var a = 0;
 var b = 0;
 var c = 0;
 
+<<<<<<< HEAD
+=======
+var c = new Coluna("coluna1");
+c.addToDo("titulo", "desc");
+
+console.log(c.listaToDo[0].titulo);
+console.log(c.listaToDo[0].descricao);
+
+
+>>>>>>> parent of c1061ca (commit)
 var id_colunas = [];
 
 function getIdColumn() {

dc76e319f04431e426947937b76937dc26635d5b
kelvins213 <kelvinotilio@gmail.com>
2022-10-30 13:46:21 -0300

    new way to connecting with MongoDB

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,24 +1,11 @@
 const mongoose = require('mongoose');
 
 const toDoListSchema = mongoose.Schema({
-    columnTitle: String,
+    //columnTitle: String,
     task: String,
     description: String,
 });
 
-module.exports = mongoose.model('ToDoList', toDoListSchema);
-
-/*
-async function setToDoListTask(task){
-    const taskInformation = {
-        columnTitle: task,
-        task: task,
-        description: task,
-    };
-    await new userSchema(taskInformation).salve();
-}
-
-export {setToDoListTask};
-*/
+module.exports = mongoose.model('todoList', toDoListSchema);
 
 
--- server.js
+++ server.js
@@ -1,24 +1,60 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://kelvins213:database@cluster0.aaknyyf.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const toDoListSchema = require('./schemas/schema');
+
+client.connect( async (err) => {
+  const collection = client.db("todolist").collection("tasks");
+  await insertData();
+  console.log("Connected to Database!");
+  client.close();
+});
+
+async function insertData(){
+    const taskInformation = {
+        //columnTitle: task,
+        task: "oi",
+        description: "oi",
+    };
+    await new toDoListSchema(taskInformation).save();
+}
+
+/*
+const mongoClient = require("mongodb").MongoClient;
+const url = "mongodb://kelvins213:database@cluster0localhost:27017";
+mongoClient.connect(url,
+    (error, connection) => {
+        if(error) return console.log(error);
+        global.connection = connection.db("todolist");
+        console.log("Connected to MongoDB");
+    });
+
+*/
+
+/*
 const express = require('express');
 const {MongoClient} = require("mongodb");
 const mongoose = require('mongoose');
-const { config } = require('process');
 const app = express();
 const toDoListSchema = require('./schemas/schema');
 
 const url = "mongodb+srv://kelvins213:database@cluster0.aaknyyf.mongodb.net/?retryWrites=true&w=majority";
-const uri = "mongodb://localhost:27017";
+const client = new MongoClient(url);
 
-async function connectMongoDB(){
-    const client = new MongoClient(uri);
-    console.log("i'm running bitch!");
+async function connectMongoDB(task){
     try{
-        await mongoose.connect(url);
-        //const db = mongoose.connection; //o atributo .connection do objeto mongoose retorna o banco de dados que você conectou
+        await client.connect();
+        const taskInformation = {
+            columnTitle: task,
+            task: task,
+            description: task,
+        };
+        await new toDoListSchema(taskInformation).save();
         console.log("Connected to MongoDB!");
     } catch (error) {
         console.error(error);
-    } finally {
-        mongoose.connection.close();
+    } finally { //pay attencion here
+        client.close();
     }
 }
 
@@ -26,13 +62,11 @@ async function setToDoListTask(task){
     //error: must be connected to a client
     //you must do the part of MongoClient stuff
     console.log("i'm running too, asshole!");
-
     const taskInformation = {
         columnTitle: task,
         task: task,
         description: task,
     };
-
     await new toDoListSchema(taskInformation).save();
 }
 
@@ -41,16 +75,8 @@ app.listen(8000, () => {
 });
 
 async function configDatabase(){
-    await connectMongoDB();
-    await setToDoListTask();
+    await connectMongoDB("Choose my president");
+    //await setToDoListTask("Choose my president");
 }
 configDatabase();
-
-
-
-
-
-
-
-
-
+*/
